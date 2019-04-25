f8167308ebda816e162ccfa5945cb60b02969e35
davidlei <davidleitw@gmail.com>
2019-04-26 01:11:40 +0800

    draw frame function

--- test.py
+++ test.py
@@ -4,6 +4,7 @@ import time
 import cv2
 import face_recognition
 import os
+SaveImg_path = r'/media/davidlei/Transcend/Beauty_recognition/beauty_recognition/test_finishdata'
 
 def Read_Img_File(Imgpath=None):
     Imgname = Imgpath
@@ -12,9 +13,10 @@ def Read_Img_File(Imgpath=None):
     # name and idx(number)
     return Imgname, ImgNumber
 
-def Drawface(Img, args):
+def Drawface(Img=None, args=(0, 0, 0, 0), ImgName=None):
     # print(len(args))
     if len(args) != 0:
+        # print('Img = {}'.format(Img))
         top = args[0][0]; right = args[0][1]; bottom = args[0][2]; left = args[0][3]
         print('top = {}, right = {}, bottom = {}, left = {}'.format(top, right, bottom, left))
         cv2.line(Img, (int((right + left)/2), top-60), (right+40, int((top + bottom)/2)), color=(255, 255, 0), thickness=5)
@@ -22,6 +24,8 @@ def Drawface(Img, args):
         cv2.line(Img, (int((right + left)/2), bottom+60), (left-40, int((top + bottom)/2)), color=(255, 255, 0), thickness=5)
         cv2.line(Img, (int((right + left)/2), bottom+60), (right+40, int((top + bottom)/2)), color=(255, 255, 0), thickness=5)
         cv2.imshow('temp', Img)
+        print(os.path.join(SaveImg_path, ImgName))
+        cv2.imwrite(os.path.join(SaveImg_path, ImgName), Img, [int(cv2.IMWRITE_JPEG_QUALITY), 100])
         cv2.waitKey(0)
         cv2.destroyAllWindows()
 
@@ -29,25 +33,25 @@ if __name__ == '__main__':
     # print(dlib.DLIB_USE_CUDA)
     # print(dlib.cuda.get_num_devices())
     path = '/media/davidlei/Transcend/Beauty_recognition/beauty_recognition/testdata/moe_five/'
-    dataset = os.listdir(r'/media/davidlei/Transcend/Beauty_recognition/beauty_recognition/testdata/moe_five/')
-    dataset.sort()
-    face = []
+    fp = open('Record.txt', 'a')
+    dataset = os.listdir(path)
+    print(os.getcwd())
     # print(len(path))
     fcount = 0
     totaltime = 0
 
     for count,idx in enumerate(dataset):
-
         ImgName, ImgNumber = Read_Img_File(idx)
         # Img = face_recognition.load_image_file(os.path.join(path, ImgName))
+        print(ImgName)
         Img = cv2.imread(os.path.join(path, ImgName))
 
         start = time.time()
         # Use cuda to do fa recognition, but i am not install dlib-cuda yet.
-        # local = face_recognition.face_locations(Img, model='cnn') => use cuda for face_recognition
+        local = face_recognition.face_locations(Img, model='cnn') # => use cuda for face_recognition
         # A list of tuples of found face locations in css (top, right, bottom, left) order
-        local = face_recognition.face_locations(Img)
-        Drawface(Img, local)
+        # local = face_recognition.face_locations(Img)
+        Drawface(Img, local, ImgName)
         end = time.time()
         totaltime = totaltime + (end - start)
 
@@ -60,5 +64,7 @@ if __name__ == '__main__':
     avgtime = totaltime / 10
     print('ac% = {}'.format((len(dataset) - fcount)/len(dataset)))
     print('average time for each picture = {}'.format(avgtime))
-
+    fp.write(" using fave_recognition.face_locations(Img, model='cnn') ")
+    fp.write('ac% = {}'.format((len(dataset) - fcount)/len(dataset)))
+    fp.write('average time for each picture = {}'.format(avgtime))
 
