d80e43da826916335888a4a214241bdda03d386d
davidlei <davidleitw@gmail.com>
2019-05-08 12:17:39 +0800

    clear the file

--- Net.py
+++ Net.py
@@ -3,10 +3,17 @@ import torch.nn as nn
 import cv2
 import os
 import numpy as np
+traindata_path = r'/media/davidlei/Transcend/Beauty_recognition/beauty_recognition/train/real__yami/'
 # https://blog.csdn.net/hbu_pig/article/details/81454503
 
 def Conv3x3(in_planes, out_planes, stride=1):
     return nn.Conv2d(in_channels=in_planes, out_channels=out_planes, kernel_size=3, stride=stride, padding=1, bias=False)
+
+def ImgtoTensor(Img):
+    # Change BGR image to [batch, in_channels, Imgsize, Imgsize]
+    Img = Img.view(3, Img.shape[0], Img.shape[1])
+    Img = torch.unsqueeze(Img, 0)
+    return Img
 
 class net(nn.Module):
     def __init__(self):
@@ -22,8 +29,21 @@ class net(nn.Module):
         self.mx3 = nn.MaxPool2d((2, 2), stride=1)
     
         # print(self.features)
-    def forward(self, Img):
-        pass
+    def forward(self, x):
+        # Conv1
+        x = self.conv1(x)
+        x = self.relu1(x)
+        x = self.mx1(x)
+        # Conv2
+        x = self.conv2(x)
+        x = self.relu2(x)
+        x = self.mx2(x)
+        # Conv3
+        x = self.conv3(x)
+        x = self.relu3(x)
+        x = self.mx3(x)
+
+        return x
     def testdata(self, Img):
         # Img = Img.view(3, 1350, 1080)
         # Img = torch.unsqueeze(Img, 1)
@@ -52,20 +72,14 @@ class net(nn.Module):
 
 if __name__ == '__main__':
     Net = net()
-    Imgpath = r'/media/davidlei/Transcend/Beauty_recognition/beauty_recognition/testdata/real__yami/'
-    Img_ls = os.listdir(Imgpath)
 
-   # Img = torch.from_numpy(cv2.imread(os.path.join(Imgpath, Img_ls[2]))).float()
-   # print(Img.shape)
-   # Img = Img.view(3, 1350, 1080)
-   # print(Img.shape)
-   # Img = torch.unsqueeze(Img, 0)
-   # Img = Img.expand(1, 3, 1350, 1080)
-    # Img = torch.reshape(Img, [16, 3, 1350, 1080])
-   # print(Img.shape)
-    # newimg = torch.ones(16, 3, 1350, 1080)
+    Img_ls = os.listdir(traindata_path)
+
+    Img = torch.from_numpy(cv2.imread(os.path.join(traindata_path, Img_ls[2]))).float()
+    Img = ImgtoTensor(Img)
+    print(Img.shape)
     # Net.testdata(Img)
-   # Net.testdata(Img)
-
+    output = Net(Img)
+    print(output.shape)
 
     # output = Net(Imgtensor)
